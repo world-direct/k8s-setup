59fa372f6734d04533b4d103a7e35fdfdd20f13c
Günter Prossliner <guenter.prossliner@world-direct.at>
2019-11-26 11:27:42 +0100

    fixes #41: Improve k8s-setup logging

--- lib/k8s-setup/main.py
+++ lib/k8s-setup/main.py
@@ -9,36 +9,47 @@ from context import Context
 from provision import Provision
 from generator import Generator
 
-def cmd_info(args):
+def main():
+
+    args=parse_args()
+    setup_logging(args)
+
+    # get execution context
+    context = Context()
+
+    commandfn=globals()["cmd_" + args.command]
+    commandfn(context, args)
+
+def cmd_info(context, args):
     logger.debug('cmd:info()')
 
     print('not implemented')
     exit(1)
 
-def cmd_checkout(args):
+def cmd_checkout(context, args):
     logger.debug('cmd:checkout(version=%s, no_deps=%i)' % (args.version, args.no_deps))
     
     print('not implemented')
     exit(1)
 
-def cmd_config(args):
+def cmd_config(context, args):
     logger.debug ('cmd:config(file=%s)' % args.file)
     context.set_file(args.file)
 
-def cmd_provision(args):
+def cmd_provision(context, args):
     logger.debug ('cmd:provision(scope=%s, ansible_options=%s)' % (args.scope, args.ansible_options))
 
     p = Provision(context)
     fn = getattr(p, args.scope)
     fn()
 
-def cmd_tool(args):
+def cmd_tool(context, args):
     logger.debug ('cmd:tool(name=%s, show_only=%d, toolargs=%s)' % (args.name, args.show_only, args.toolargs))
     tool = Tool(context)
     rc = tool.run(args.name, args.toolargs)
     exit(rc)
 
-def cmd_generate(args):
+def cmd_generate(context, args):
     logger.debug ('cmd:generate(type=%s, merge=%d)' % (args.type, args.merge))
     generator = Generator(context)
 
@@ -47,13 +58,43 @@ def cmd_generate(args):
 
     exit(rc)
 
-res=parse_args()
+def setup_logging(args):
+    """Set up the logging."""
+    """https://www.programcreek.com/python/example/357/logging.WARNING"""
 
-if res.debug:
-    logging.basicConfig(level=logging.DEBUG)
+    level = logging.DEBUG if args.debug else logging.INFO
+    fmt = "%(asctime)s %(levelname)s [%(name)s] %(message)s" \
+            if args.debug else \
+        "%(asctime)s %(levelname)s %(message)s"
 
-# get execution context
-context = Context()
+    logging.basicConfig(level=level)
 
-commandfn=globals()["cmd_" + res.command]
-commandfn(res)
+    colorfmt = "%(log_color)s{}%(reset)s".format(fmt)
+    datefmt = '%Y-%m-%d %H:%M:%S'
+
+    # Suppress overly verbose logs from libraries that aren't helpful
+    logging.getLogger('requests').setLevel(logging.WARNING)
+    logging.getLogger('urllib3').setLevel(logging.WARNING)
+    logging.getLogger('aiohttp.access').setLevel(logging.WARNING)
+
+    try:
+        from colorlog import ColoredFormatter
+        logging.getLogger().handlers[0].setFormatter(ColoredFormatter(
+            colorfmt,
+            datefmt=datefmt,
+            reset=True,
+            log_colors={
+                'DEBUG': 'cyan',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red',
+            }
+        ))
+    except ImportError:
+        pass
+
+    logger = logging.getLogger('')
+    logger.setLevel(level) 
+
+main()
