edb8cc263275a3716dc2fee46a3b11c177a10465
Günter Prossliner <guenter.prossliner@world-direct.at>
2019-12-06 13:42:00 +0100

    #79: CA generation and certmanager ClusterIssuer deployment

---
+++ lib/k8s-setup/cert.py
@@ -0,0 +1,55 @@
+
+
+
+def generate_selfsigned_ca(clustername):
+    """
+    Generates a selfsiged ca certificate and it's private key
+    Returns a tuple of two strings: crt, key
+    """
+
+    from datetime import datetime, timedelta
+    import ipaddress
+
+    from cryptography import x509
+    from cryptography.x509.oid import NameOID
+    from cryptography.hazmat.primitives import hashes
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives import serialization
+    from cryptography.hazmat.primitives.asymmetric import rsa
+    
+    # Generate key
+    key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend(),
+    )
+    
+    name = x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, unicode(clustername))
+    ])
+ 
+    # path_len=1 means that this certificate can sign one level of sub-certs
+    basic_contraints = x509.BasicConstraints(ca=True, path_length=1)
+    now = datetime.utcnow()
+    cert = (
+        x509.CertificateBuilder()
+        .subject_name(name)
+        .issuer_name(name)
+        .public_key(key.public_key())
+        .serial_number(1)
+        .not_valid_before(now)
+        .not_valid_after(now + timedelta(days=10*365))
+        .add_extension(basic_contraints, False)
+        .sign(key, hashes.SHA256(), default_backend())
+    )
+
+    cert_pem = cert.public_bytes(encoding=serialization.Encoding.PEM)
+
+    key_pem = key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+
+    return cert_pem, key_pem
+
--- lib/k8s-setup/provision.py
+++ lib/k8s-setup/provision.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import logging
+import os
+import shutil
 
 from tool import Tool
 
@@ -33,6 +35,57 @@ class Provision():
 
     def incluster(self):
         logger.info("Provisioning scope 'incluster'")
+
+        # check for the CA certificate, if 'k8s_certs_mode' == 'CA'
+        k8s_certs_mode = self.context.config['k8s_certs_mode']
+        logger.debug("k8s_certs_mode=%s" % k8s_certs_mode)
+
+        if(k8s_certs_mode == 'CA'):
+            import cert
+
+            crtpath = None
+            keypath = None
+
+            if self.context.config['k8s_certs_ca']['generate']:
+                crtpath = ".local/cacrt.pem"
+                keypath = ".local/cakey.pem"
+                logger.debug("Check if %s and %s exists" % (crtpath, keypath))
+
+                if os.path.isfile(crtpath) and os.path.isfile(keypath):
+                    logger.info("CA already generated in %s %s" % (crtpath, keypath))
+                else:
+                    logger.info("Generate CA files to %s %s" % (crtpath, keypath))
+                    crt, key = cert.generate_selfsigned_ca("generated-ca.k8stest.local")
+
+                    with open(crtpath, "w") as f:
+                        f.write(crt)
+
+                    with open(keypath, "w") as f:
+                        f.write(key)
+
+            else:
+                crtpath=self.context.config["k8s_certs_ca"]["crt_filepath"]
+                keypath=self.context.config["k8s_certs_ca"]["key_filepath"]
+
+                if os.path.isfile(crtpath) and os.path.isfile(keypath):
+                    logger.info("Using configured ca-files: %s %s" % (crtpath, keypath))
+
+                    # TODO: validate that the files are ok
+
+            # symlink the files to the chart, because helm can't access files
+            # outside of the chart directory
+            cafilespath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../charts/wd-certmanager/.ca")
+            cafilespath = os.path.relpath(cafilespath)
+            logger.debug("Using path %s to symlink ca-files" % cafilespath)
+
+            # always recreate links
+            if os.path.isdir(cafilespath):
+                shutil.rmtree(cafilespath)
+
+            # the .ca file is in .gitignore, so it's ok to keep the (for debugging)
+            os.mkdir(cafilespath)
+            os.symlink(os.path.abspath(crtpath), os.path.join(cafilespath, "cacrt.pem"))
+            os.symlink(os.path.abspath(keypath), os.path.join(cafilespath, "cakey.pem"))
 
         tool = Tool(self.context)
         tool.ansible_playbook_auto("./lib/ansible/incluster.yml", add_localhost=True, become=False)
